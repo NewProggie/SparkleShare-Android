8fd029314457ecf3b821507ed86e304432a6a981
Kai Wolf <kai.wolf@gmail.com>
2011-10-05 15:04:38 +0200

    Implemented deleting user account

--- src/org/sparkleshare/android/SettingsActivity.java
+++ src/org/sparkleshare/android/SettingsActivity.java
@@ -1,17 +1,59 @@
 package org.sparkleshare.android;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.ContextWrapper;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 
 public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 
+	private Preference releaseAccount;
+	private Context context;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		context = this;
 		addPreferencesFromResource(R.xml.settings);
+		
+		releaseAccount = (Preference) findPreference(getString(R.string.settings_release_account));
+		releaseAccount.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				AlertDialog.Builder builder = new AlertDialog.Builder(context);
+				builder.setMessage(getString(R.string.release_account_hint))
+					.setTitle(getString(R.string.are_you_sure))
+					.setCancelable(false)
+					.setPositiveButton(getString(R.string.release), new DialogInterface.OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							SharedPreferences prefs = SettingsActivity.getSettings((ContextWrapper) context);
+							Editor editor = prefs.edit();
+							editor.remove("ident");
+							editor.remove("authCode");
+							editor.commit();
+						}
+					}).setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							dialog.dismiss();
+						}
+					});
+				AlertDialog alert = builder.create();
+				alert.show();
+				return true;
+			}
+		});
 	}
 	
 	public static final SharedPreferences getSettings(final ContextWrapper context) {
