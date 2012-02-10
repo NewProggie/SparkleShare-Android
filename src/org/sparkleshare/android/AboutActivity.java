a982dcf6bef6af351b479ac1fbfa5564def7a71b
Kai Wolf <kai.wolf@gmail.com>
2012-02-10 20:18:27 +0100

    Rewrote AboutActivity using Fragments

--- src/org/sparkleshare/android/AboutActivity.java
+++ src/org/sparkleshare/android/AboutActivity.java
@@ -1,21 +1,18 @@
 package org.sparkleshare.android;
 
-import org.sparkleshare.android.ui.BaseActivity;
-
-import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 /**
  * Provides basic information on how to use SparkleShare and this android app
  * @author kai
  *
  */
-public class AboutActivity extends BaseActivity {
+public class AboutActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.about);
-		setupActionBar(getString(R.string.info), Color.WHITE);
+		setContentView(R.layout.about_fragment);
 	}
 }
---
+++ src/org/sparkleshare/android/ui/AboutFragment.java
@@ -0,0 +1,25 @@
+package org.sparkleshare.android.ui;
+
+import org.sparkleshare.android.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AboutFragment extends Fragment {
+
+	private static final String TAG = "AboutFragment";
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.about, container, false);
+	}
+}
