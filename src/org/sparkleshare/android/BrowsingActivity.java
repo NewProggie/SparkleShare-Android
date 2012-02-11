9dc915340a06319e5007f2071a23b153f3dcb957
Kai Wolf <kai.wolf@gmail.com>
2012-02-11 16:38:25 +0100

    Added context menu to BrowsingActivity

--- src/org/sparkleshare/android/BrowsingActivity.java
+++ src/org/sparkleshare/android/BrowsingActivity.java
@@ -2,8 +2,12 @@ package org.sparkleshare.android;
 
 import org.sparkleshare.android.actionbarcompat.ActionBarActivity;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 /**
  * Activity for browsing content of a SparkleShare-Dashboard instance.
  * @author kai
@@ -22,6 +26,24 @@ public class BrowsingActivity extends ActionBarActivity {
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
 		super.onConfigurationChanged(newConfig);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater menuInflater = getMenuInflater();
+		menuInflater.inflate(R.menu.option, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.opt_settings:
+			Intent settings = new Intent(this, SettingsActivity.class);
+			startActivity(settings);
+			break;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 	
 }
